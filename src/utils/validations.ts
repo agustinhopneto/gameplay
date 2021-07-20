f7060d13002c8e69de045974a4c6ce88302c0659
agustinhopneto <agustinho.pneto@gmail.com>
2021-07-20 18:22:48 -0300

    Fix datetime validation

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -6,6 +6,22 @@ export const validateDateTime = (
   hour: string,
   minutes: string,
 ): void => {
+  if (Number(day) < 1 || Number(day) > 31) {
+    throw new Error('Ops... Dia inválido!');
+  }
+
+  if (Number(month) < 1 || Number(month) > 12) {
+    throw new Error('Ops... Mês inválido!');
+  }
+
+  if (Number(hour) < 0 || Number(hour) > 23) {
+    throw new Error('Ops... Hora inválida!');
+  }
+
+  if (Number(minutes) < 0 || Number(minutes) > 59) {
+    throw new Error('Ops... Minutos inválidos!');
+  }
+
   const now = dayjs();
   const date = dayjs(
     `${new Date().getFullYear()}-${month}-${day} ${hour}:${minutes}`,
